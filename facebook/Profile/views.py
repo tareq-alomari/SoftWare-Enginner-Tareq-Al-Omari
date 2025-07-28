fbde3a19e3bcd0dbfcf63ec21befb9152613226c
Tareq Al_Omari <qutabh2@gmail.com>
2025-07-29 01:09:35 +0300

    رفع جميع الملفات من الجهاز واستبدال الريبو البعيد

--- facebook/Profile/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-def home(request):
-    # pass return HttpResponse("صفحة الملف الشخصي (Profile)")
-    return render(request, 'Profile/home.html')
