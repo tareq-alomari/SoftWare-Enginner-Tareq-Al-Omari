388bb3bbf7dee504d4df24bf4a10f26aebf88ce7
Tareq Al-Omari <145411789+tareq-alomari@users.noreply.github.com>
2025-09-15 23:38:33 +0300

    Update settings.py

--- Labs/Lab8/student_management/student_management/settings.py
+++ Labs/Lab8/student_management/student_management/settings.py
@@ -57,8 +57,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'qutabh2@gmail.com' 
-EMAIL_HOST_PASSWORD = 'ncqc bczm whaz uwhd' # **هام: استبدل هذا بكلمة مرور التطبيقات من جوجل**
+EMAIL_HOST_USER = '@gmail.com' 
+EMAIL_HOST_PASSWORD = '' # **هام: استبدل هذا بكلمة مرور التطبيقات من جوجل**
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
